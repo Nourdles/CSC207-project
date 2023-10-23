4bcc1e9e2d99c991e9c1c76ebc7da61232bb0106
ably2003 <albert.li0133@gmail.com>
2023-10-23 12:01:32 -0400

    Added listing (except pictures attribute)

---
+++ src/entity/Listing.java
@@ -0,0 +1,40 @@
+package entity;
+
+import java.time.LocalDateTime;
+
+public class Listing {
+    private final Book book;
+    private final LocalDateTime creationTime;
+
+    private double listingPrice;
+
+    private String condition;
+
+    Listing(Book book, double listingPrice, String condition, LocalDateTime creationTime) {
+        this.book = book;
+        this.creationTime = creationTime;
+        this.listingPrice = listingPrice;
+        this.condition = condition;
+
+        /* Add attribute for book photos*/
+
+    }
+
+    public double getPrice(){
+        return listingPrice;
+    }
+
+    public void setPrice(double newPrice){
+        this.listingPrice = newPrice;
+    }
+
+
+    public String getCondition(){
+        return this.condition;
+    }
+
+    public void setCondition(String newCondition){
+        this.condition = newCondition;
+    }
+
+}
