dafb5355d497ef5c1de6e3e97267034ff57292f9
Nourdles <gattinour9@gmail.com>
2023-10-23 12:06:54 -0400

    Added setters to Book Corrected import in Listing

--- src/entity/Book.java
+++ src/entity/Book.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 
 public class Book {
 
-    private final String title;
-    private final String genre;
-    private final int year;
-    private final String author;
-    private final String summary;
-    private final int ISBN;
-    private final int inStock;
+    private String title;
+    private String genre;
+    private int year;
+    private String author;
+    private String summary;
+    private int ISBN;
+    private int inStock;
 
     Book(String title, String genre, int year, String author, String summary, int ISBN, int inStock) {
         this.title = title;
@@ -26,12 +26,24 @@ public class Book {
         return title;
     }
 
+    public void setTitle(String title){
+        this.title = title;
+    }
+
     public String getGenre() {
         return genre;
     }
 
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
     public int getYear() {
         return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 
 
@@ -39,15 +51,31 @@ public class Book {
         return author;
     }
 
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
     public String getSummary() {
         return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
     }
 
     public int getISBN() {
         return ISBN;
     }
 
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
     public int getInStock() {
         return inStock;
+    }
+
+    public void setInStock(int inStock) {
+        this.inStock = inStock;
     }
 }
