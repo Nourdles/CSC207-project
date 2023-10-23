f0261e42b1fe7632ee7ee60cd512b09c5a3b111d
Nourdles <gattinour9@gmail.com>
2023-10-23 11:42:28 -0400

    Added attributes and constructor to Book Class

--- src/entity/Book.java
+++ src/entity/Book.java
@@ -1,4 +1,26 @@
 package src.entity;
 
-public interface Book {
+import java.util.ArrayList;
+
+public class Book {
+
+    private final String title;
+    private final String genre;
+    private final int year;
+    private final String author;
+    private final String summary;
+    private final int ISBN;
+    private final int inStock;
+
+    Book(String title, String genre, int year, String author, String summary, int ISBN, int inStock) {
+        this.title = title;
+        this.genre = genre;
+        this.year = year;
+        this.author = author;
+        this.summary = summary;
+        this.ISBN = ISBN;
+        this.inStock = inStock;
+    }
+
+
 }
