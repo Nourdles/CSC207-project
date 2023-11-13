aae9f0fe377d19027a01fabcbc8ef3aa3ec7eb5b
ably2003 <albert.li0133@gmail.com>
2023-11-13 13:00:58 -0500

    added empty classes, added login and signup but with some bugs

--- src/entity/CommonUserFactory.java
+++ src/entity/CommonUserFactory.java
@@ -1,4 +1,15 @@
 package entity;
+public class CommonUserFactory implements UserFactory {
+        /**
+         * Requires: password is valid.
+         * @param name
+         * @param password
+         * @return
+         */
+    @Override
+    public User create(String name, String password, ) {
+        return new CommonUser(name, password);
+    }
+}
 
-public class CommonUserFactory {
 }
