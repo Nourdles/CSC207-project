2a5f8af526fb6f02b8e285c03f2db63d4b71a179
ably2003 <albert.li0133@gmail.com>
2023-12-03 10:17:59 -0500

    Trying to revert
    temp

--- src/use_case/listings/ListingsDataAccessInterface.java
+++
@@ -1,9 +0,0 @@
-package use_case.listings;
-
-import entity.Listing;
-
-import java.util.List;
-
-public interface ListingsDataAccessInterface {
- List<Listing> getUserListings(String username);
-}
---
+++ src/view/ListingsView.java
@@ -0,0 +1,27 @@
+package view;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class ListingsView extends JPanel implements ActionListener, PropertyChangeListener {
+
+    /**
+     * A view of a seller's own listings.
+     */
+    public final String viewName = "listings view";
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
+
+    // private final
+}
