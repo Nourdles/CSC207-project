fa312fa5aba73a166eede22e735a2373285051a9
Nour Gatti <gattinour9@gmail.com>
2023-12-04 04:41:24 -0500

    reverted changes that caused issues in delete listings and listings view

--- src/use_case/listings/ListingsDataAccessInterface.java
+++ src/use_case/listings/ListingsDataAccessInterface.java
@@ -1,4 +1,9 @@
 package use_case.listings;
 
+import entity.Listing;
+
+import java.util.List;
+
 public interface ListingsDataAccessInterface {
+    List<Listing> getUserListings(String username);
 }
